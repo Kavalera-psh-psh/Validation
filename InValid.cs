3bedf355fcd00305284fdb663fe295a5963eacee
Kavalera-psh-psh <kioshira312002@mail.ru>
2022-12-14 20:00:06 +0300

    1

---
+++ InValid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunNikMethodInClass
+{
+    class InValid
+    {
+        public bool Validation(string Name, string Log, string Pass, string ContPass, string Email, string ContEmail)
+        {
+            bool result = true;
+            if (Name == "")
+            {
+                result = false;
+            }
+            if (Log == "")
+            {
+                result = false;
+            }
+            if (Pass == "")
+            {
+                result = false;
+            }
+            if (ContPass == "")
+            {
+                result = false;
+            }
+            if (Email == "")
+            {
+                result = false;
+            }
+            if (ContEmail == "")
+            {
+                result = false;
+            }
+            if (Name != "" && Log != "" && Pass != "" && ContPass != "" && Email != "" && ContEmail != "")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace TunNikMethodInClass
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        InValid val = new InValid();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            string log = tbLogin.Text;
+            string pass = tbPass.Password;
+            string conpass = tbRepPass.Text;
+            string em = tbEmail.Text;
+            string conem = tbRepEmail.Text;
+            
+            if (val.Validation(name, log, pass, conpass, em, conem))
+            {
+                MessageBox.Show("Поздравляю с регистрацией!");
+            }
+            else 
+            {
+                MessageBox.Show("Не все поля верно заполнены!");
+            }
+        }
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
